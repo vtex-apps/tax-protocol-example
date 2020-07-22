ded464d246070c44cbb9a3c8959c2cf5ebc09e26
Fabiana Fonseca <fabiana.fonseca@vtex.com.br>
2020-07-22 20:57:26 -0300

    adding missing typings

--- node/parsers/vtexToProvider.ts
+++ node/parsers/vtexToProvider.ts
@@ -4,23 +4,48 @@ export const parseVtexToProvider = (checkoutRequest: CheckoutRequest) => {
 }
 
 interface CheckoutRequest {
+  orderFormId: string
+  salesChannel: string
   items: Item[]
+  clientEmail: string
   clientData: Client
   shippingDestination: ShippingInformation
-  orderFormId: string
+  totals: Total[]
+  paymentData: Payment[]
 }
 
 interface Item {
   id: string
   sku: string
+  ean: string
+  refId: string
+  unitMultiplier: number
+  measurementUnit: string
+  targetPrice: number
   itemPrice: number
+  quantity: number
   discountPrice: number | null
   dockId: string
   freightPrice: number
+  brandId: string
 }
 
 interface Client {
   email: string
+}
+
+interface Total {
+  id: string
+  name: string
+  value: number
+}
+
+interface Payment {
+  paumentSystem: string
+  bin: string
+  referenceValue: number
+  value: number
+  installments?: number
 }
 
 interface ShippingInformation {
